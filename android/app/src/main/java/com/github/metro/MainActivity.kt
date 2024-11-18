cfddcc78a5bb4f4478b8ec14b894fc0acfc58b75
luis10barbo <luis10barbo2020@gmail.com>
2024-11-18 17:59:28 -0300

    feat: Pesquisar estacoes proximas em atividade main

--- android/app/src/main/java/com/github/metro/MainActivity.kt
+++ android/app/src/main/java/com/github/metro/MainActivity.kt
@@ -2,11 +2,19 @@ package com.github.metro
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.github.metro.constantes.ConstantesApi
 import com.github.metro.constantes.ConstantesExtra
 import com.github.metro.databinding.MainLayoutBinding
 import com.github.metro.models.LocalPesquisa
+import com.github.metro.utils.ConexaoVolley
+import com.github.metro.utils.LocalizacaoUtils
 import com.github.metro.viewmodels.MainActivityViewModel
 
 class MainActivity : ComponentActivity() {
@@ -29,8 +37,46 @@ class MainActivity : ComponentActivity() {
             pesquisaActivityIntent.putExtra(ConstantesExtra.VALOR_PESQUISA_EXTRA, textoPesquisa)
             startActivity(pesquisaActivityIntent)
         }
-        val intent = Intent(this, RotaPesquisaActivity::class.java)
-        intent.putExtra(ConstantesExtra.LOCAL_PESQUISA_EXTRA, LocalPesquisa(nome="Centro Universitário Estácio do Recife", lat=-8.062037, lon=-34.9161162, endereco="Av. Eng. Abdias de Carvalho, 1678 - Madalena Recife - PE, 50720-225, Brazil"))
-        startActivity(intent)
+//        val intent = Intent(this, RotaPesquisaActivity::class.java)
+//        intent.putExtra(ConstantesExtra.LOCAL_PESQUISA_EXTRA, LocalPesquisa(nome="Centro Universitário Estácio do Recife", lat=-8.062037, lon=-34.9161162, endereco="Av. Eng. Abdias de Carvalho, 1678 - Madalena Recife - PE, 50720-225, Brazil"))
+//        startActivity(intent)
+        setupRvEstacoesProximas()
+        pesquisarEstacoesProximas()
+    }
+
+    fun setupRvEstacoesProximas() {
+        binding.rvEstacoesProximas.layoutManager = LinearLayoutManager(this)
+    }
+
+    fun mostrarEstacoesProximas(estacoes: ArrayList<LocalPesquisa>) {
+        estacoes
+    }
+
+    fun pesquisarEstacoesProximas() {
+        val localizacao = LocalizacaoUtils.getLocation(this)!!
+        val textoPesquisa = binding.etPesquisa.text.toString()
+
+        val request = JsonObjectRequest(
+            Request.Method.GET, "${ConstantesApi.CAMINHO_STATION}?localLat=${localizacao.latitude}&localLon=${localizacao.longitude}&type=bus_station", null,
+            { response ->
+                response
+                val resultados = response.getJSONArray("results")
+                val resultadosList = ArrayList<LocalPesquisa>()
+                for (i in 0 until resultados.length()) {
+                    val resultado = resultados.getJSONObject(i)
+                    val geometry = resultado.getJSONObject("geometry")
+                    val location = geometry.getJSONObject("location")
+                    val lat = location.getDouble("lat")
+                    val lon = location.getDouble("lng")
+                    val name = resultado.getString("name")
+                    resultadosList.add(LocalPesquisa(nome = name, lat = lat, lon = lon, endereco = ""))
+                }
+                mostrarEstacoesProximas(resultadosList)
+            },
+            {
+                    error ->
+                Log.e("request erro", error.toString())
+            })
+        ConexaoVolley.getInstance(this).addToRequestQueue(request)
     }
 }
