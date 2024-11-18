843c760ce6d3909d9611b22cf126bf400eca8727
luis10barbo <luis10barbo2020@gmail.com>
2024-11-18 17:28:14 -0300

    fix: Mostrar apenas tipos de veiculo validos

--- back-end/api/src/localidade/model/vehicle-type.enum.ts
+++ back-end/api/src/localidade/model/vehicle-type.enum.ts
@@ -1,19 +1,7 @@
 export enum VehicleType {
     CAR = 'car',
     BUS = 'bus',
-    TRUCK = 'truck',
-    MOTORCYCLE = 'motorcycle',
-    BICYCLE = 'bicycle',
-    CAR_RENTAL = 'car_rental',
-    BUS_STATION = 'bus_station',
-    TRAIN_STATION = 'train_station',
-    TRANSIT_STATION = 'transit_station',
-    TAXI_STAND = 'taxi_stand',
-    GAS_STATION = 'gas_station',
-    CAR_WASH = 'car_wash',
-    PARKING = 'parking',
-    AIRPORT = 'airport',
-    FERRY_TERMINAL = 'ferry_terminal',
-    LIMOUSINE_SERVICE = 'limousine_service',
-    RIDE_SHARING = 'ride_sharing'
+    TRAIN = 'train',
+    TRAM = 'tram',
+    RAIL = 'rail',
 }
