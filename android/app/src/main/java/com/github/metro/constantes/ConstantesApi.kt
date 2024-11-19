5a62f10150321c6714f9c8ce72b537a4b82f0eae
LucasLima004 <lucaslimasl004@gmail.com>
2024-11-19 12:34:16 -0300

    refactor: Alteração de URI da constante.

--- android/app/src/main/java/com/github/metro/constantes/ConstantesApi.kt
+++ android/app/src/main/java/com/github/metro/constantes/ConstantesApi.kt
@@ -2,7 +2,7 @@ package com.github.metro.constantes
 
 class ConstantesApi {
     companion object {
-        private val CAMINHO_BASE = "http://192.168.0.195:3000"
+        private val CAMINHO_BASE = "https://android.luisbrb.com.br/"
         val CAMINHO_LOCALIDADE = CAMINHO_BASE + "/localidade"
         val CAMINHO_STATION = CAMINHO_LOCALIDADE + "/station"
         val CAMINHO_SEARCH = CAMINHO_LOCALIDADE + "/search"
